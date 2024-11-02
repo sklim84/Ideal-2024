d3762bde5ec31f4656d57bb061170f4ac05bf19c
Ubuntu <ubuntu@ip-172-31-38-42.ap-northeast-2.compute.internal>
2024-11-02 08:06:39 +0000

    modify make_syn_data_by_local_data

--- federated_learning/1_split_dataset.py
+++ federated_learning/1_split_dataset.py
@@ -10,8 +10,9 @@ grouped_data = data.groupby('HNDE_BANK_RPTV_CODE')
 
 # Save each group as a separate CSV file
 for name, group in grouped_data:
-    output_file = f'./datasets/DATOP_HF_TRANS_{name}.csv'
-    group.to_csv(output_file, index=False)
+    if len(group) >= 5:
+        output_file = f'./datasets/DATOP_HF_TRANS_{name}.csv'
+        group.to_csv(output_file, index=False)
 
 output_file_names = [f'DATOP_HF_TRANS_{name}.csv' for name in grouped_data.groups.keys()]
 print(output_file_names)
